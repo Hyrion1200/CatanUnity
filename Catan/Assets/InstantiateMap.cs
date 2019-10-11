b76873fcef92b289dee89d47fb5c608e8da936b0
De Souza <nathansds@live.fr>
2019-10-11 23:23:11 +0200

    Debug genere_map

--- Catan/Assets/InstantiateMap.cs
+++ Catan/Assets/InstantiateMap.cs
@@ -22,11 +22,35 @@ public class InstantiateMap : MonoBehaviour
     int z = -2;
     int i = 0;
     int pair = 0;
-    void generator_map(int i, int x, int z, int pair)
+    System.Random aleatoire = new System.Random();
+  
+    void generator_map(float i, float x, float z, int pair, int generator)
     {
+        switch (i)
+        {
+            case 1: x = -0.5f; z = 0; break;
+            case 2: x = 0.5f; z = 0; break;
+            case 3: x = 1.5f; z = 0; break;
+            case 4: x = 2.5f; z = 0; break;
+            case 5: x = 3.5f; z = 0; break;
+            case 6: x = 0; z = 1; break;
+            case 7: x = 1; z = 1; break;
+            case 8: x = 2; z = 1; break;
+            case 9: x = 3; z = 1; break;
+            case 10: x = 0; z = -1; break;
+            case 11: x = 1; z = -1; break;
+            case 12: x = 2; z = -1; break;
+            case 13: x = 3; z = -1; break;
+            case 14: x = 0.5f; z = 2; break;
+            case 15: x = 1.5f; z = 2; break;
+            case 16: x = 2.5f; z = 2; break;
+            case 17: x = 0.5f; z = -2; break;
+            case 18: x = 1.5f; z = -2; break;
+            case 19: x = 2.5f; z = -2; break;
 
-        System.Random aleatoire = new System.Random();
-        int generator = aleatoire.Next(6);
+        }
+            
+             
         if (i == 20) { }
         else if (generator == 0 & desert > 0)
         {
@@ -34,11 +58,11 @@ public class InstantiateMap : MonoBehaviour
             Instantiate(desert_map, new Vector3(x, 0, z), Quaternion.identity);
             if (pair == 1)
             {
-                generator_map(i + 1, x + 1, z, pair - 1);
+                generator_map(i + 1, x, z, pair - 1, aleatoire.Next(6));
             }
             else
             {
-                generator_map(i + 1, x, z + 1, pair + 1);
+                generator_map(i + 1, x, z, pair + 1, aleatoire.Next(6));
             }
         }
 
@@ -48,11 +72,11 @@ public class InstantiateMap : MonoBehaviour
             Instantiate(brick_map, new Vector3(x, 0, z), Quaternion.identity);
             if (pair == 1)
             {
-                generator_map(i + 1, x + 1, z, pair - 1);
+                generator_map(i + 1, x, z, pair - 1, aleatoire.Next(6));
             }
             else
             {
-                generator_map(i + 1, x, z + 1, pair + 1);
+                generator_map(i + 1, x, z, pair + 1, aleatoire.Next(6));
             }
         }
         else if (generator == 2 & wood > 0)
@@ -61,11 +85,11 @@ public class InstantiateMap : MonoBehaviour
             Instantiate(wood_map, new Vector3(x, 0, z), Quaternion.identity);
             if (pair == 1)
             {
-                generator_map(i + 1, x + 1, z, pair - 1);
+                generator_map(i + 1, x, z, pair - 1, aleatoire.Next(6));
             }
             else
             {
-                generator_map(i + 1, x, z + 1, pair + 1);
+                generator_map(i + 1, x, z, pair + 1, aleatoire.Next(6));
             }
         }
 
@@ -75,11 +99,11 @@ public class InstantiateMap : MonoBehaviour
             Instantiate(stone_map, new Vector3(x, 0, z), Quaternion.identity);
             if (pair == 1)
             {
-                generator_map(i + 1, x + 1, z, pair - 1);
+                generator_map(i + 1, x, z, pair - 1, aleatoire.Next(6));
             }
             else
             {
-                generator_map(i + 1, x, z + 1, pair + 1);
+                generator_map(i + 1, x, z, pair + 1, aleatoire.Next(6));
             }
         }
 
@@ -89,11 +113,11 @@ public class InstantiateMap : MonoBehaviour
             Instantiate(sheep_map, new Vector3(x, 0, z), Quaternion.identity);
             if (pair == 1)
             {
-                generator_map(i + 1, x + 1, z, pair - 1);
+                generator_map(i + 1, x, z, pair - 1, aleatoire.Next(6));
             }
             else
             {
-                generator_map(i + 1, x, z + 1, pair + 1);
+                generator_map(i + 1, x, z, pair + 1, aleatoire.Next(6));
             }
         }
 
@@ -103,17 +127,21 @@ public class InstantiateMap : MonoBehaviour
             Instantiate(straw_map, new Vector3(x, 0, z), Quaternion.identity);
             if (pair == 1)
             {
-                generator_map(i + 1, x + 1, z, pair - 1);
+                generator_map(i + 1, x, z, pair - 1, aleatoire.Next(6));
             }
             else
             {
-                generator_map(i + 1, x, z + 1, pair + 1);
+                generator_map(i + 1, x, z, pair + 1, aleatoire.Next(6));
             }
+        }
+        else
+        {
+            generator_map(i, x, z, pair, aleatoire.Next(6));
         }
     }
     void Start()
     {
-        generator_map(0, 0, -2, 0);
+        generator_map(1, 0, -2, 0, aleatoire.Next(6));
     }
 
 
