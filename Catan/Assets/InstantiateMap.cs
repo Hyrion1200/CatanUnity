1c831e3eac57bd73fc6f5f524640f87b49126750
De Souza <nathansds@live.fr>
2019-10-11 21:07:19 +0200

    Test fonction generator_map

--- Catan/Assets/InstantiateMap.cs
+++ Catan/Assets/InstantiateMap.cs
@@ -7,188 +7,112 @@ public class InstantiateMap : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform prefab;
-    void Start()
-    {   
-        int desert = 1;
-        int brick = 3;
-        int wood = 4;
-        int stone = 3;
-        int sheep = 4;
-        int straw = 4;
-        int x;
-        int z;
-        void generator_map()
-        {
 
-            int i = 0;
-            while (i <= 19)
+    int desert = 1;
+    int brick = 3;
+    int wood = 4;
+    int stone = 3;
+    int sheep = 4;
+    int straw = 4;
+    int x = -3;
+    int z = -3;
+    int i = 0;
+    int pair = 0;
+    void generator_map(int i, int x, int z, int pair)
+    {
+
+            System.Random aleatoire = new System.Random();
+            int generator = aleatoire.Next(6);
+            if(i = 20) { }
+            else if (generator == 0 & desert > 0)
             {
-                if (i == 1)
-                {
-                    x = 0;
-                    z = 0;
-                }
+                desert--;
+                Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
+            if (pair = 1)
+            {
+                generator_map(i + 1, x + 1, z, pair - 1);
+            }
+            else
+            {
+                generator_map(i + 1, x, z + 1, pair + 1);
+            }
+            }
 
-                if (i == 2)
-                {
-                    x = 1;
-                    z = 0;
-                }
-
-                if (i == 3)
-                {
-                    x = 2;
-                    z = 0;
-                }
-
-                if (i == 4)
-                {
-                    x = 3;
-                    z = 0;
-                }
-
-                if (i == 5)
-                {
-                    x = 4;
-                    z = 0;
-                }
-
-                if (i == 5)
-                {
-                    x = 4;
-                    z = 0;
-                }
-
-                if (i == 6)
-                {
-                    x = 0;
-                    z = 1;
-                }
-
-                if (i == 7)
-                {
-                    x = 1;
-                    z = 1;
-                }
-
-                if (i == 8)
-                {
-                    x = 2;
-                    z = 1;
-                }
-
-                if (i == 9)
-                {
-                    x = 3;
-                    z = 1;
-                }
-
-                if (i == 10)
-                {
-                    x = 0;
-                    z = -1;
-                }
-
-                if (i == 11)
-                {
-                    x = 1;
-                    z = -1;
-                }
-
-                if (i == 12)
-                {
-                    x = 2;
-                    z = -1;
-                }
-
-                if (i == 13)
-                {
-                    x = 3;
-                    z = -1;
-                }
-
-                if (i == 14)
-                {
-                    x = 0;
-                    z = 2;
-                }
-
-                if (i == 15)
-                {
-                    x = 1;
-                    z = 2;
-                }
-
-                if (i == 16)
-                {
-                    x = 2;
-                    z = 2;
-                }
-
-                if (i == 17)
-                {
-                    x = 0;
-                    z = -2;
-                }
-
-                if (i == 18)
-                {
-                    x = 1;
-                    z = -2;
-                }
-
-                if (i == 19)
-                {
-                    x = 2;
-                    z = -2;
-                }
-
-                System.Random aleatoire = new System.Random();
-                int generator = aleatoire.Next(6);
-
-                if (generator == 0 & desert > 0)
-                {
-                    desert--;
-                    Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
-                }
-
-                if (generator == 1 & brick > 0)
-                {
-                    brick--;
-                    Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
-                }
-
-                if (generator == 2 & wood > 0)
-                {
-                    wood--;
-                    Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
-                }
-
-                if (generator == 3 & stone > 0)
-                {
-                    stone--;
-                    Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
-                }
-
-                if (generator == 4 & sheep > 0)
-                {
-                    sheep--;
-                    Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
-                }
-
-                if (generator == 5 & straw > 0)
-                {
-                    straw--;
-                    Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
-                }
-
-                
-                else
-                {
-                    generator_map();
-                }
-                i++;
-            };
+            else if (generator == 1 & brick > 0)
+            {
+                brick--;
+                Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
+            if (pair = 1)
+            {
+                generator_map(i + 1, x + 1, z, pair - 1);
+            }
+            else
+            {
+                generator_map(i + 1, x, z + 1, pair + 1);
+            }
         }
-    }
+
+            else if (generator == 2 & wood > 0)
+            {
+                wood--;
+                Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
+            if (pair = 1)
+            {
+                generator_map(i + 1, x + 1, z, pair - 1);
+            }
+            else
+            {
+                generator_map(i + 1, x, z + 1, pair + 1);
+            }
+        }
+
+            else if (generator == 3 & stone > 0)
+            {
+                stone--;
+                Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
+            if (pair = 1)
+            {
+                generator_map(i + 1, x + 1, z, pair - 1);
+            }
+            else
+            {
+                generator_map(i + 1, x, z + 1, pair + 1);
+            }
+        }
+
+            else if (generator == 4 & sheep > 0)
+            {
+                sheep--;
+                Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
+            if (pair = 1)
+            {
+                generator_map(i + 1, x + 1, z, pair - 1);
+            }
+            else
+            {
+                generator_map(i + 1, x, z + 1, pair + 1);
+            }
+        }
+
+            else if (generator == 5 & straw > 0)
+            {
+                straw--;
+                Instantiate(prefab, new Vector3(x, 0, z), Quaternion.identity);
+            if (pair = 1)
+            {
+                generator_map(i + 1, x + 1, z, pair - 1);
+            }
+            else
+            {
+                generator_map(i + 1, x, z + 1, pair + 1);
+            }
+        }
+
+
+        }
+        void Start()
+        {
+            generator_map(0, -3, -3, 0);
+        }
+    
 }
